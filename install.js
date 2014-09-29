b3fba5ddec80dafe975d8b1a109b1440c6d40586
Glenn Block <glenn.block@gmail.com>
2014-09-28 17:36:51 -0700

    Adding installing Roslyn dependencies

--- install.js
+++ install.js
@@ -5,13 +5,35 @@ var unzip = require('unzip');
 var fs = require('fs');
 var path = require('path');
 
-var response = request("http://chocolatey.org/api/v2/package/ScriptCs/" + version);
+var getScriptCs = function() {
+	var response = request("http://chocolatey.org/api/v2/package/ScriptCs/" + version);
 
-response.pipe(unzip.Extract({path:"scs"}));
+	response.pipe(unzip.Extract({path:"scs"}));
 
-response.on("end", function(){
-	copyDir(path.join("scs","tools","scriptcs"), "lib");
-});
+	response.on("end", function(){
+		copyDir(path.join("scs","tools","scriptcs"), "lib");
+	});
+}
+
+var getRoslynCompilersCSharp = function() {
+	var response = request("http://nuget.org/api/v2/package/Roslyn.Compilers.CSharp/1.2.20906.2");
+
+	response.pipe(unzip.Extract({path:"roslyn1"}));
+
+	response.on("end", function(){
+		copyDir(path.join("roslyn1","lib","net45"), "lib");
+	});
+}
+
+var getRoslynCompilersCommon = function() {
+	var response = request("http://nuget.org/api/v2/package/Roslyn.Compilers.Common/1.2.20906.2");
+
+	response.pipe(unzip.Extract({path:"roslyn2"}));
+
+	response.on("end", function(){
+		copyDir(path.join("roslyn2","lib","net45"), "lib");
+	});
+}
 
 var copyDir = function(src, dest) {
 	var files = fs.readdirSync(src);
@@ -31,3 +53,7 @@ var copyDir = function(src, dest) {
 var copy = function(src, dest) {
 	fs.createReadStream(src).pipe(fs.createWriteStream(dest));
 };
+
+getScriptCs();
+getRoslynCompilersCSharp();
+getRoslynCompilersCommon();
