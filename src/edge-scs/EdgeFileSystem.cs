67d66b2a645d4c8e0d5114a751c689eb3de42dea
Glenn Block <glenn.block@gmail.com>
2014-09-28 23:21:43 -0700

    Adding missing EdgeFileSystem

---
+++ src/edge-scs/EdgeFileSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptCs;
+
+namespace EdgeScs
+{
+    public class EdgeFileSystem : FileSystem
+    {
+        public EdgeFileSystem()
+        {
+            _hostBin = Path.GetDirectoryName(typeof (EdgeCompiler).Assembly.Location);
+        }
+
+        private string _hostBin;
+
+        public override string HostBin
+        {
+            get { return _hostBin; }
+        }
+
+        public override string ModulesFolder
+        {
+            get { return ""; }
+        }
+    }
+}
